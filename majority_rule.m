9140d6ecec3b8ee719e067f31579b2babe5c5f9c
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-11-12 13:48:06 +0000

    Add files via upload

---
+++ majority_rule.m
@@ -0,0 +1,18 @@
+function [Label] = majority_rule[Labels]
+Length = length(Labels);
+M0 = 0;
+M1 = 0;
+for i = 1:Length
+    if Labels(i,1) == 0
+        M0 = M0 + 1;
+    elseif Labels(i,1) == 1
+        M1 = M1 + 1;
+    end
+end
+if M0 > M1
+    Label = 0;
+elseif M1 > M0
+    Label = 1;
+else
+    Label = [];
+end
