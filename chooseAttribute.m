00fba1a246c10cfa403ab98490a1f4893a8de96b
kan98 <34004612+kan98@users.noreply.github.com>
2019-11-14 14:15:27 +0000

    Create chooseAttribute.m

---
+++ chooseAttribute.m
@@ -0,0 +1,21 @@
+function [best_attribute, best_threshold] = chooseAttribute(features ,targets)
+    maxGain = 0;
+    label = targets (:, 1);
+    for i = 1:size(features, 2)
+        usingColumn = features(:, i);
+        
+        minValue = min(usingColumn);
+        maxValue = max(usingColumn);
+        step = (maxValue - minValue) / 98;
+        
+        for j = minValue:step:maxValue
+            gainValue = gains(usingColumn, label, j);
+            
+            if gainValue > maxGain
+                maxGain = gainValue;
+                best_attribute = i;
+                best_threshold = j;
+            end
+        end
+    end
+end
