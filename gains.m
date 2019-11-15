2d3e23c4ccc4be960bb10fbc905c9d77405dacc6
Roderick Bertuzzi-Glover <psyrb2@nottingham.ac.uk>
2019-11-15 19:41:27 +0000

    Improved the gains function

--- gains.m
+++ gains.m
@@ -1,26 +1,35 @@
-function s = gain(A)
-    sp = 0; 
-    sn = 0;
+function s = gains(data_column, labels, threshold) % Gain Function
+    size = length(labels);
     
-    for i = 1:length(A)
-        sp = sp + A(i, 1);
-        sn = sn + A(i, 2);
+    % Sum of "Postive" and "Negative" examples
+    sP = 0;
+    sN = 0;
+    
+    for i = 1:size
+        if labels(i) == 1
+            sP = sP + 1;
+        else
+            sN = sN + 1;
+        end
     end
-
-    s = I(sp, sn) - remainder(A, sp, sn);
+    
+    s = I(sP, sN, size) - remainder(A);
 end
 
-function s = I(p, n)
-    s = -(p / (p + n)) * (log2((p / (p + n)) - (n / (p + n))) * log2((n / (p + n))));
-end
-
-function s = remainder(A, sp, sn)
+function s = remainder(A) % Remainder Function
+    V = length(A);
     s = 0;
     
-    for i = 1:length(A)
-        p = A(i, 1);
-        n = A(i, 2);
-       
-        s = s + (((p + n) / (sp + sn)) * I(p, n));
+    for i = 1:V
+        P = A(i, 1);
+        N = A(i, 2);
+        
+        s = s + ((P + N) / V) * I(P, N, V);
     end
+end
+
+function s = I(P, N, size) % Entropy Function
+    P = (P / size); % Ratio of + 
+    N = (N / size); % Ratio of -
+    s = - P * log2(P) - N * log2(N);
 end
