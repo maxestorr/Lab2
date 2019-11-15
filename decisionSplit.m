642f42edd425eeea487f00a4d3b1b616cbc67e89
maxestorr <maxestorr@gmail.com>
2019-11-15 21:13:41 +0000

    Splits the data at each node

---
+++ decisionSplit.m
@@ -0,0 +1,14 @@
+function [split1, split2, split1Labels, split2Labels] = decisionSplit(features, labels, threshold)
+split1 = []; split1Labels = [];
+split2 = []; split2Labels = [];
+
+for row=1:length(features)
+    if norm(row) < norm(threshold)
+        split1(end+1) = features(row);
+        split1Labels(end+1) = labels(row);
+    else
+        split2(end+1) = features(row);
+        split2Labels(end+1) = labels(row);
+    end
+end
+end
