ceaa778f6e52c1ec8c848028ddcd348b3e7f342d
maxestorr <maxestorr@gmail.com>
2019-11-17 22:14:48 +0000

    Corrected big stupid mistake

--- decisionSplit.m
+++ decisionSplit.m
@@ -1,13 +1,13 @@
-function [split1, split2, split1Labels, split2Labels] = decisionSplit(features, labels, threshold)
+function [split1, split2, split1Labels, split2Labels] = decisionSplit(dataColumn, labels, threshold)
 split1 = []; split1Labels = [];
 split2 = []; split2Labels = [];
 
-for row=1:length(features)
-    if norm(features(row, :)) < norm(threshold)
-        split1(end+1, :) = features(row, :);
+for row=1:length(dataColumn)
+    if dataColumn(row, :) < threshold
+        split1(end+1, :) = dataColumn(row, :);
         split1Labels(end+1) = labels(row);
     else
-        split2(end+1, :) = features(row, :);
+        split2(end+1, :) = dataColumn(row, :);
         split2Labels(end+1) = labels(row);
     end
 end
