28934db5d4a516c66306b5b48d20b469de736005
maxestorr <maxestorr@gmail.com>
2019-11-17 22:09:54 +0000

    Modified remainder func to split data

--- gains.m
+++ gains.m
@@ -18,13 +18,24 @@ end
 
 function s = remainder(data_column, threshold) % Remainder Function
     V = length(data_column);
+    s1 = 0;
+    s2 = 0;
     s = 0;
     
     for i = 1:V
-        P = data_column(i, 1);
-        N = data_column(i, 2);
+        [~, ~, split1Labels, split2Labels] = decisionSplit(data_column, threshold);
         
-        s = s + ((P + N) / V) * entropy(P, N, V);
+        P1 = numel(split1Labels(split1Labels == 1));
+        N1 = numel(split1Labels(split1Labels == 0));
+        P2 = numel(split2Labels(split2Labels == 1));
+        N2 = numel(split2Labels(split2Labels == 0));
+        
+        V1 = P1 + N1;
+        V2 = P2 + N2;
+
+        s1 = s1 + ((P1 + N1) / V) * entropy(P1, N1, V1);
+        s2 = s2 + ((P2 + N2) / V) * entropy(P2, N2, V2);
+        s = s1 + s2;
     end
 end
 
