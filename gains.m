58f610a81ce1b0ba3642a0aa89c5e5409fd83279
Max Storr <ppxms6@nottingham.ac.uk>
2019-11-18 15:15:01 +0000

    Refactored to use decisionSplit and labels struct from decisionSplit

--- gains.m
+++ gains.m
@@ -1,43 +1,47 @@
-function gain = gains(data_column, labels, threshold) % Gain Function
-    size = length(labels);
+function [gain, dataSplits, labels] = gains(data_column, labels_column, threshold) % Gain Function
+    size = length(labels_column);
     
     % Sum of "Postive" and "Negative" examples
-    sP = numel(labels(labels == 1));
-    sN = numel(labels(labels == 0));
+    sP = numel(labels_column(labels_column == 1));
+    sN = numel(labels_column(labels_column == 0));
     
-    gain = entropy(sP, sN, size) - remainder(data_column, labels, threshold);
+    entropyVal = entropy(sP, sN, size);
+    [remainderVal, dataSplits, labels] = remainder(data_column, labels_column, threshold);
+    gain = entropyVal - remainderVal;
 end
 
-function remainder = remainder(data_column, labels, threshold) % Remainder Function
+function [remainderVal, dataSplits, labels] = remainder(data_column, labels_column, threshold)
     V = length(data_column);
     s1 = 0;
     s2 = 0;
-    remainder = 0;
+    remainderVal = 0;
     
     for i = 1:V
-        [~, ~, split1Labels, split2Labels] = decisionSplit(data_column, labels, threshold);
+        [dataSplits, labels] = decisionSplit(data_column, labels_column, threshold);
         
-        P1 = numel(split1Labels(split1Labels == 1));
-        N1 = numel(split1Labels(split1Labels == 0));
-        P2 = numel(split2Labels(split2Labels == 1));
-        N2 = numel(split2Labels(split2Labels == 0));
+        % Find number of positive and negative examples in left and right
+        % branches
+        P1 = numel(labels.left(labels.left == 1));
+        N1 = numel(labels.left(labels.left == 0));
+        P2 = numel(labels.right(labels.right == 1));
+        N2 = numel(labels.right(labels.right == 0));
         
         V1 = P1 + N1;
         V2 = P2 + N2;
 
         s1 = s1 + ((P1 + N1) / V) * entropy(P1, N1, V1);
         s2 = s2 + ((P2 + N2) / V) * entropy(P2, N2, V2);
-        remainder = s1 + s2;
+        remainderVal = s1 + s2;
     end
 end
 
-function entropy = entropy(P, N, size) % Entropy Function
+function entropyVal = entropy(P, N, size) % Entropy Function
     P = (P / size); % Ratio of + 
     N = (N / size); % Ratio of -
     
     if P == 0 || N == 0
-        entropy = 0;
+        entropyVal = 0;
     else
-        entropy = - P * log2(P) - N * log2(N);
+        entropyVal = - P * log2(P) - N * log2(N);
     end
 end
