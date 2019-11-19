6eda14000699181601754a2209f4cd7bcab4450f
maxestorr <maxestorr@gmail.com>
2019-11-19 09:38:52 +0000

    Added comment explaining the function

--- kFold.m
+++ kFold.m
@@ -1,4 +1,5 @@
 function output = kFold(k, Labels)
+    % Output k n.o. arrays containing the indices for the respective fold.
     data_length = length(Labels);
     A = 1:data_length;
     A = A(randperm(length(A)));
