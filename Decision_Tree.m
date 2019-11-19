ee804d4dbdfd80035c9a493192d3c9b997ec87b3
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-11-19 08:07:12 +0000

    Update Decision_Tree.m

--- Decision_Tree.m
+++ Decision_Tree.m
@@ -1,25 +1,25 @@
 function [Tree] = Decision_Tree(Features,Labels)
 Tree = struct('op', {}, 'kids', {}, 'class', {}, 'attribute', {}, 'threshold', {}); 
 if size(unique(Labels), 1) == 1
-    Tree.op = [];
-    Tree.kids = [];
-    Tree.class = majority_rule(Labels);
-    Tree.attribute = [];
-    Tree.threshold = [];
+    Tree(1).op = [];
+    Tree(1).kids = [];
+    Tree(1).class = majority_rule(Labels);
+    Tree(1).attribute = [];
+    Tree(1).threshold = [];
 else
     %from attribute testing function, need the chosen attribute, its index,
     %the chosen threshold, and the X,Y,L data in the new splits
     [Attribute, Threshold,dataSplits, labels] = chooseAttribute(Features,Labels);
     if Attribute < 50
-        Tree.op = 'x' + string(Attribute);
+        Tree(1).op = 'x' + string(Attribute);
     else
-        Tree.op = 'x' + string(Attribute-49);
+        Tree(1).op = 'y' + string(Attribute-49);
     end
-    Tree.op = Attribute;
-    Tree.class = [];
-    Tree.attribute = Attribute;
-    Tree.threshold = Threshold;
-    Tree.kids = {Decision_Tree(dataSplits.left,labels.left),Decision_Tree(dataSplits.right,labels.right)};
+    Tree(1).op = Attribute;
+    Tree(1).class = [];
+    Tree(1).attribute = Attribute;
+    Tree(1).threshold = Threshold;
+    Tree(1).kids = {Decision_Tree(dataSplits.left,labels.left),Decision_Tree(dataSplits.right,labels.right)};
 end
 
     
