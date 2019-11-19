7b07061403ae41164fb770df512ee2f09afe0e16
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-11-19 07:00:07 +0000

    Update Decision_Tree.m

--- Decision_Tree.m
+++ Decision_Tree.m
@@ -9,14 +9,18 @@ if size(unique(Labels), 1) == 1
 else
     %from attribute testing function, need the chosen attribute, its index,
     %the chosen threshold, and the X,Y,L data in the new splits
-    [Attribute, threshold,Features1,Features2,Labels1,Labels2] = attribute_test(Features,Labels);
+    [Attribute, Threshold,dataSplits, labels] = chooseAttribute(Features,Labels);
+    if Attribute < 50
+        Tree.op = 'x' + str(Attribute);
+    else
+        Tree.op = 'x' + str(Attribute-49);
+    end
     Tree.op = Attribute;
     Tree.class = [];
-    Tree.attribute = Index;
+    Tree.attribute = Attribute;
     Tree.threshold = Threshold;
-    Tree.kids = {Decision_Tree(Features1,Labels1),Decision_Tree(Features2,Labels2)};
+    Tree.kids = {Decision_Tree(dataSplits.left,labels.left),Decision_Tree(dataSplits.right,labels.right)};
 end
 
     
 end
-
