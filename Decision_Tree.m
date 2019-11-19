40580901233ec9dd94ab44f1cc723b01e731122d
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-11-19 10:05:38 +0000

    Add files via upload

--- Decision_Tree.m
+++ Decision_Tree.m
@@ -10,12 +10,12 @@ else
     %from attribute testing function, need the chosen attribute, its index,
     %the chosen threshold, and the X,Y,L data in the new splits
     [Attribute, Threshold,dataSplits, labels] = chooseAttribute(Features,Labels);
-    if Attribute < 50
-        Tree(1).op = 'x' + string(Attribute);
+    if Attribute < 50 
+        out_string = 'x'+string(Attribute)+'>'+string(Threshold);
     else
-        Tree(1).op = 'y' + string(Attribute-49);
+        out_string = 'y'+string(Attribute-49)+'>'+string(Threshold);
     end
-    Tree(1).op = Attribute;
+    Tree(1).op = out_string;
     Tree(1).class = [];
     Tree(1).attribute = Attribute;
     Tree(1).threshold = Threshold;
