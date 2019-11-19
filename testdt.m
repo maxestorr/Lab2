d517bcff2b02f98fab8fe7088d3087623d9e9e33
BurgundyIsAPublicEnemy <veryseriousemailaddress@gmail.com>
2019-11-19 10:28:40 +0000

    Update testdt.m

--- testdt.m
+++ testdt.m
@@ -1,12 +1,12 @@
 function predictions = testdt(tree, testfeatures)
     %create array of 0s for tests
-    predictions = zeros(size(testfeatures, 1), 1);
-     for i = 1 : size(testfeatures, 1)
+    predictions = zeros(length(testfeatures));
+     for i = 1 : length(testfeatures)
         
         leaf = tree;
         
         while isempty(leaf.class)
-            if features(i, leaf.attribute) < leaf.threshold
+            if testfeatures(i, leaf.attribute) < leaf.threshold
                 leaf = leaf.kids{1};
             else
                 leaf = leaf.kids{2};
