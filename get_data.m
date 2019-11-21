b2f7531e48694fe81deac6fbf01a7d98d7e928f2
Martin-Benedict <56830707+Martin-Benedict@users.noreply.github.com>
2019-11-21 18:57:16 +0000

    Comments

--- get_data.m
+++ get_data.m
@@ -1,4 +1,6 @@
 function [Features, Labels] = get_data()
+%Function loads the data and labels from the .csv files, combines the X and Y data into a single array
+%and exports them to the main program
     DataX = table2array(readtable('predx_for_classification.csv'));
     DataY = table2array(readtable('predy_for_classification.csv'));
     Labels = table2array(readtable('label.csv'));
