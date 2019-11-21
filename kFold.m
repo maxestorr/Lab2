687fcc17e0d84eff4e1b8d2dd6ca62f40b7e4ab8
kan98 <34004612+kan98@users.noreply.github.com>
2019-11-21 21:50:38 +0000

    Implemented k-fold (#17)

--- kFold.m
+++ kFold.m
@@ -1,12 +1,22 @@
-function output = kFold(k, Labels)
-    data_length = length(Labels);
+function [new_features, new_labels] = kFold(k, features, labels)
+    data_length = length(labels);
     A = 1:data_length;
     A = A(randperm(length(A)));
         
     remainder_value = mod(data_length, k);
     A = A(1: data_length-remainder_value);
     
-    main_array = reshape(A, [], k);
-        
-    output = main_array();
+    indices_array = reshape(A, [], k);
+    
+    new_features = zeros(length(indices_array),size(features,2), k);
+    new_labels = zeros(length(indices_array), k);
+
+    for i = 1:k
+        for j = 1:length(indices_array)
+            for k = 1:size(features,2)
+                new_features(j,k,i) = features(indices_array(j, i), k);
+            end
+            new_labels(j,i) = labels(indices_array(j,i));
+        end
+    end
 end
