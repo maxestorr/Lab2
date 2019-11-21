fc76e24f94eb453a7c534c894f18621eda41fa6f
kan98 <34004612+kan98@users.noreply.github.com>
2019-11-21 16:58:53 +0000

    Implemented k-fold

--- main.m
+++ main.m
@@ -4,31 +4,47 @@ clc;
 
 [features, labels] = get_data();
 
-%features = features(1:1000, :);
-%labels = labels(1:1000, :);
+% CHANGE 1 TO WHICHEVER LABEL CLASS IS BEING USED
+labels = labels(:,1);
+    
+% features = features(1:1000, :);
+% labels = labels(1:1000, :);
 
 k_sliceNum = 10;
-kslices = cvpartition(size(features, 1), 'KFold', k_sliceNum);
+[new_features, new_labels] = kFold(k_sliceNum, features, labels);
 
 accuracyArray = zeros(k_sliceNum, 1);
 fscoreArray = zeros(k_sliceNum, 1);
-total_fMeasureScore = 0;  
+total_fMeasureScore = 0;
 
-for i = 1 : kslices.NumTestSets
-    trainIndices = kslices.training(i);
-    testIndices = kslices.test(i);
+for i = 1 : k_sliceNum
+    feature_test = new_features(:,:, i);
+    label_test = new_labels(:, i);
+    clearvars feature_train label_train
+    for j = 1 : k_sliceNum
+        if i ~= j
+            if exist('feature_train','var') == 1
+                feature_train = cat(1, feature_train, new_features(:,:, j));
+            else
+                feature_train = new_features(:,:, j);
+            end
+            if exist('label_train','var') == 1
+                label_train = cat(1, label_train, new_labels(:, j));
+            else
+                label_train = new_labels(:, j);
+            end
+        end
+    end
 
-
-    tree = Decision_Tree(features(trainIndices, :), labels(trainIndices, :));
+    tree = Decision_Tree(feature_train, label_train);
     DrawDecisionTree(tree,'test')
 
-    predictions = testdt(tree, features(testIndices, :) );
-    actual = labels(testIndices, :);
-    [fMeasureScore, precisionValue, recallValue] = fMeasure(actual, predictions);
+    predictions = testdt(tree, feature_test);
+    [fMeasureScore, precisionValue, recallValue] = fMeasure(label_test, predictions);
     
     total_fMeasureScore = total_fMeasureScore + fMeasureScore;
     fscoreArray(i) = fMeasureScore;
-    accuracyArray(i) = sum(predictions == labels(testIndices)) / (size(features, 1) / k_sliceNum);
+    accuracyArray(i) = sum(predictions == label_test) / (size(features, 1) / k_sliceNum);
     
 
 end
